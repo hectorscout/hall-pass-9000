6dd48230db0e80e1765293133abd2583c1bf8fa1
The Batman <hectorscout@hotmail.com>
2022-08-27 23:23:44 -0600

    set maxAge and comment out createUserSession

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -10,6 +10,7 @@ export const sessionStorage = createCookieSessionStorage({
   cookie: {
     name: "__session",
     httpOnly: true,
+    maxAge: 60 * 24 * 30, // 30 days
     path: "/",
     sameSite: "lax",
     secrets: [process.env.SESSION_SECRET],
@@ -63,29 +64,29 @@ export async function requireUser(request: Request) {
   throw await logout(request);
 }
 
-export async function createUserSession({
-  request,
-  userId,
-  remember,
-  redirectTo,
-}: {
-  request: Request;
-  userId: string;
-  remember: boolean;
-  redirectTo: string;
-}) {
-  const session = await getSession(request);
-  session.set(USER_SESSION_KEY, userId);
-  return redirect(redirectTo, {
-    headers: {
-      "Set-Cookie": await sessionStorage.commitSession(session, {
-        maxAge: remember
-          ? 60 * 60 * 24 * 7 // 7 days
-          : undefined,
-      }),
-    },
-  });
-}
+// export async function createUserSession({
+//   request,
+//   userId,
+//   remember,
+//   redirectTo,
+// }: {
+//   request: Request;
+//   userId: string;
+//   remember: boolean;
+//   redirectTo: string;
+// }) {
+//   const session = await getSession(request);
+//   session.set(USER_SESSION_KEY, userId);
+//   return redirect(redirectTo, {
+//     headers: {
+//       "Set-Cookie": await sessionStorage.commitSession(session, {
+//         maxAge: remember
+//           ? 60 * 60 * 24 * 7 // 7 days
+//           : undefined,
+//       }),
+//     },
+//   });
+// }
 
 export async function logout(request: Request) {
   const session = await getSession(request);
