71590c5272f7d5a28e40e5cd88516b2fd95fb323
The Batman <hectorscout@hotmail.com>
2022-08-27 22:18:14 -0600

    sketchy prisma migrate stuff

--- prisma/migrations/20220828012612_add_student_and_pass/migration.sql
+++ prisma/migrations/20220828012612_add_student_and_pass/migration.sql
@@ -45,12 +45,12 @@ CREATE TABLE "new_User" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "externalId" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "displayName" TEXT NOT NULL,
+    "displayName" TEXT NOT NULL DEFAULT '',
     "profileImgUrl" TEXT,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
 );
-INSERT INTO "new_User" ("createdAt", "email", "id", "updatedAt") SELECT "createdAt", "email", "id", "updatedAt" FROM "User";
+-- INSERT INTO "new_User" ("createdAt", "email", "id", "updatedAt") SELECT "createdAt", "email", "id", "updatedAt" FROM "User";
 DROP TABLE "User";
 ALTER TABLE "new_User" RENAME TO "User";
 CREATE UNIQUE INDEX "User_externalId_key" ON "User"("externalId");
