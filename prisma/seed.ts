70a301438013b826d1ddbe8d768d69c76f211408
The Batman <hectorscout@hotmail.com>
2022-08-27 19:47:03 -0600

    schema update with Student and Pass

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,28 +1,22 @@
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcryptjs";
+// import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
 async function seed() {
-  const email = "rachel@remix.run";
-
-  // cleanup the existing database
-  await prisma.user.delete({ where: { email } }).catch(() => {
-    // no worries if it doesn't exist yet
-  });
-
-  const hashedPassword = await bcrypt.hash("racheliscool", 10);
-
-  const user = await prisma.user.create({
-    data: {
-      email,
-      password: {
-        create: {
-          hash: hashedPassword,
-        },
-      },
-    },
-  });
+  // const email = "rachel@remix.run";
+  //
+  // // cleanup the existing database
+  // await prisma.user.delete({ where: { email } }).catch(() => {
+  //   // no worries if it doesn't exist yet
+  // });
+  //
+  // const user = await prisma.user.create({
+  //   data: {
+  //     email,
+  //     },
+  //   },
+  // });
 
   console.log(`Database has been seeded. 🌱`);
 }
