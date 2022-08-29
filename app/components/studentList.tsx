a3652f17c20f0057b0c2c78d1715573c1f2f9590
The Batman <hectorscout@hotmail.com>
2022-08-28 18:32:31 -0600

    highlight the selected student

--- app/components/studentList.tsx
+++ app/components/studentList.tsx
@@ -1,14 +1,18 @@
-import { Link } from "@remix-run/react";
-// import { SerializedStudents } from "~/routes/__index";
+import { Link, useParams } from "@remix-run/react";
 import type { getStudents } from "~/models/hall-pass.server";
 interface HeaderProps {
   students: Awaited<ReturnType<typeof getStudents>>;
 }
 export const StudentList: React.FC<HeaderProps> = ({ students }) => {
+  const { studentId } = useParams();
+
   return (
     <ol className="mr-10">
       {students.map((student) => (
-        <li key={student.id}>
+        <li
+          key={student.id}
+          className={studentId === student.id ? "bg-amber-200" : undefined}
+        >
           <Link to={`${student.id}`} className="flex">
             <div className="flex-1">{`${student.firstName} ${student.lastName}`}</div>
             <div>{student.period}</div>
