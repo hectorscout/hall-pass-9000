107b9ef06b8d787bee7cc64988dbfb6d6fc05731
The Batman <hectorscout@hotmail.com>
2022-09-13 19:02:37 -0600

    fix lint warning

--- app/components/studentList.tsx
+++ app/components/studentList.tsx
@@ -1,5 +1,6 @@
 import { Link, useParams } from "@remix-run/react";
-import { MouseEventHandler, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import type { MouseEventHandler } from "react";
 import { EyeSlashIcon } from "@heroicons/react/20/solid";
 import { capitalizeString, getPassStatus } from "~/utils/utils";
 import { Button } from "~/components/common/button";
