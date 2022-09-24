63116043edaff8a227252bd4bd82abbd1296013d
The Batman <hectorscout@hotmail.com>
2022-09-23 22:03:23 -0600

    ugh, with the lint

--- app/routes/__index/settings.tsx
+++ app/routes/__index/settings.tsx
@@ -1,6 +1,7 @@
 import { Form, useActionData, useTransition } from "@remix-run/react";
 import { Button } from "~/components/common/button";
-import type { ActionFunction, json } from "@remix-run/node";
+import type { ActionFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import { requireUserId } from "~/utils/session.server";
 import { upsertSetting } from "~/models/settings.server";
 import invariant from "tiny-invariant";
