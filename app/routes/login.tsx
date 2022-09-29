5705d9a1ed8266528fde1abbef41466cba37c8a3
The Batman <hectorscout@hotmail.com>
2022-09-29 16:14:49 -0600

    privacy policy link on login page

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -3,6 +3,7 @@ import { json, redirect } from "@remix-run/node";
 import * as React from "react";
 
 import { getUserId } from "~/utils/session.server";
+import { Link } from "@remix-run/react";
 
 export async function loader({ request }: LoaderArgs) {
   const userId = await getUserId(request);
@@ -53,34 +54,9 @@ export default function LoginPage() {
           </div>
         </div>
       </div>
+      <Link to="/privacy" className="absolute right-0 bottom-0 m-5">
+        Privacy Policy
+      </Link>
     </main>
   );
 }
-// export default function LoginPage() {
-//   return (
-//     <>
-//       <div className="flex min-h-full">
-//         <div className="flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-//           <div className="mx-auto w-full max-w-sm lg:w-96">
-//             <h2 className="mt-6 text-3xl font-extrabold text-gray-900">🪵 into your account</h2>
-//
-//             <div className="mt-6">
-//               <form action="/auth/google" method="POST" className="space-y-6">
-//                 <button className="w-full" type="submit" kind="black" size="lg">
-//                   {/*<Github className="mr-2 h-5 w-5" /> Sign in with Github*/} login
-//                 </button>
-//               </form>
-//             </div>
-//           </div>
-//         </div>
-//         <div className="relative hidden w-0 flex-1 lg:block">
-//           <img
-//             className="absolute inset-0 h-full w-full object-cover"
-//             src="https://images.unsplash.com/photo-1527430253228-e93688616381?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NjE1NzIyNzc&ixlib=rb-1.2.1&q=80"
-//             alt=""
-//           />
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
