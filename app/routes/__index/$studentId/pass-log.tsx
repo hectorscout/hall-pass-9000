1c87bdbf399818f301daec77b430ea557a63ab92
The Batman <hectorscout@hotmail.com>
2022-10-01 00:02:34 -0600

    remove pass-log

--- app/routes/__index/$studentId/pass-log.tsx
+++
@@ -1,9 +0,0 @@
-import { HallPassLog } from "~/components/hallPassLog/hallPassLog";
-
-export default function PassLog() {
-  return (
-    <div className="absolute right-0 top-0 z-10 flex h-full w-1/3 bg-gray-500 px-10 text-gray-300">
-      tacos
-    </div>
-  );
-}
