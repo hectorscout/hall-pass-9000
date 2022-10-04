126f2cd060d37264b98e9394e42665c1d2902a2a
The Batman <hectorscout@hotmail.com>
2022-10-03 22:26:36 -0600

    clean up DurationInput with a render function

--- app/components/durationInput.tsx
+++ app/components/durationInput.tsx
@@ -3,6 +3,34 @@ interface DurationInputProps {
   updateDuration: Function;
 }
 
+const renderInput = ({
+  value,
+  min,
+  max,
+  onValueChange,
+}: {
+  value?: number;
+  min?: number;
+  max?: number;
+  onValueChange: Function;
+}) => {
+  return (
+    <label className="font-mono text-gray-900">
+      {(value ?? 0) < 10 ? "0" : null}
+      <input
+        className={`relative appearance-none border-none bg-gray-100 p-1 outline-none ${
+          (value ?? 0) < 10 ? "w-14" : "w-24"
+        }`}
+        type="number"
+        min={min}
+        max={max}
+        value={value}
+        onChange={({ target: { value } }) => onValueChange(parseInt(value))}
+      />
+    </label>
+  );
+};
+
 export const DurationInput = ({
   duration,
   updateDuration,
@@ -11,68 +39,33 @@ export const DurationInput = ({
     <div className="text-6xl text-gray-900">
       {duration.days ? (
         <>
-          <label className="font-mono text-gray-900">
-            {(duration.days ?? 0) < 10 ? "0" : null}
-            <input
-              className={`relative appearance-none border-none bg-gray-100 p-1 outline-none ${
-                (duration.days ?? 0) < 10 ? "w-14" : "w-24"
-              }`}
-              type="number"
-              min="0"
-              value={duration.days}
-              onChange={({ target: { value } }) =>
-                updateDuration({ days: parseInt(value) })
-              }
-            />
-          </label>{" "}
+          {renderInput({
+            value: duration.days,
+            min: 0,
+            max: 28,
+            onValueChange: (value: number) => updateDuration({ days: value }),
+          })}
           :
         </>
       ) : null}
-      <label className="font-mono text-gray-900">
-        {(duration.hours ?? 0) < 10 ? "0" : null}
-        <input
-          className={`relative appearance-none border-none bg-gray-100 p-1 outline-none ${
-            (duration.hours ?? 0) < 10 ? "w-14" : "w-24"
-          }`}
-          type="number"
-          max="23"
-          min={duration.days === 0 ? "0" : undefined}
-          value={duration.hours}
-          onChange={({ target: { value } }) =>
-            updateDuration({ hours: parseInt(value) })
-          }
-        />
-      </label>
+      {renderInput({
+        value: duration.hours,
+        min: duration.days === 0 ? 0 : undefined,
+        max: 23,
+        onValueChange: (value: number) => updateDuration({ hours: value }),
+      })}
       :
-      <label className="font-mono text-gray-900">
-        {(duration.minutes ?? 0) < 10 ? "0" : null}
-        <input
-          className={`relative appearance-none border-none bg-gray-100 p-1 outline-none ${
-            (duration.minutes ?? 0) < 10 ? "w-14" : "w-24"
-          }`}
-          type="number"
-          min={duration.hours === 0 ? "0" : undefined}
-          value={duration.minutes}
-          onChange={({ target: { value } }) =>
-            updateDuration({ minutes: parseInt(value) })
-          }
-        />
-      </label>
+      {renderInput({
+        value: duration.minutes,
+        min: duration.hours === 0 ? 0 : undefined,
+        onValueChange: (value: number) => updateDuration({ minutes: value }),
+      })}
       :
-      <label className="font-mono text-gray-900">
-        {(duration.seconds ?? 0) < 10 ? "0" : null}
-        <input
-          className={`relative appearance-none border-none bg-gray-100 p-1 outline-none ${
-            (duration.seconds ?? 0) < 10 ? "w-14" : "w-24"
-          }`}
-          type="number"
-          min={duration.minutes === 0 ? "0" : undefined}
-          value={duration.seconds}
-          onChange={({ target: { value } }) =>
-            updateDuration({ seconds: parseInt(value) })
-          }
-        />
-      </label>
+      {renderInput({
+        value: duration.seconds,
+        min: duration.minutes === 0 ? 0 : undefined,
+        onValueChange: (value: number) => updateDuration({ seconds: value }),
+      })}
     </div>
   );
 };
