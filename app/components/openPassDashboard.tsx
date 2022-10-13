2c1724e9f327e5d784bd6ec8704862e5e9017236
The Batman <hectorscout@hotmail.com>
2022-10-12 22:50:48 -0600

    break out OxygenLevel component

--- app/components/openPassDashboard.tsx
+++ app/components/openPassDashboard.tsx
@@ -1,11 +1,8 @@
-import { formatDurationDigital, getDurationStatus } from "~/utils/utils";
+import { formatDurationDigital } from "~/utils/utils";
 import { useUserSettings } from "~/hooks/useUserSettings";
 import { Button } from "~/components/common/button";
-import {
-  CheckBadgeIcon,
-  ExclamationTriangleIcon,
-} from "@heroicons/react/24/outline";
-import { Warning } from "postcss";
+import { CheckBadgeIcon } from "@heroicons/react/24/outline";
+import { OxygenLevel } from "~/components/oxygenLevel";
 
 interface OpenPassDashboardProps {
   openPassId: String;
@@ -13,12 +10,6 @@ interface OpenPassDashboardProps {
   oxygenLevel: number;
   isPersonal: boolean;
 }
-
-const statusMessages = {
-  good: "",
-  warning: "Warning",
-  critical: "Critical!!",
-};
 
 // TODO: move to a component (or fix), share with PassButton
 const rocketIcon = (
@@ -44,16 +35,6 @@ export const OpenPassDashboard = ({
   oxygenLevel,
   isPersonal,
 }: OpenPassDashboardProps) => {
-  const userSettings = useUserSettings();
-  const status = getDurationStatus(elapsedDuration, userSettings);
-  const warningSeconds = userSettings.warning * 60;
-  const criticalSeconds = userSettings.critical * 60;
-  const oxygenPercent = Math.max((oxygenLevel / criticalSeconds) * 100, 0);
-  const warningPercent = Math.min(
-    oxygenPercent,
-    (1 - warningSeconds / criticalSeconds) * 100
-  );
-
   return (
     <div
       className={`flex w-1/2 flex-col gap-10 rounded-2xl bg-gray-800/80 p-5 text-gray-300`}
@@ -65,37 +46,10 @@ export const OpenPassDashboard = ({
       <div className="flex items-center justify-center font-mono text-9xl">
         {formatDurationDigital(elapsedDuration)}
       </div>
-      <div className={`flex flex-col gap-5`}>
-        <div className="flex justify-between text-3xl">
-          <div className="py-1">Oxygen Level:</div>
-          {status !== "good" ? (
-            <div
-              className={`animate-pulse bg-${status} flex items-center gap-2 rounded-full px-3 py-1`}
-            >
-              <ExclamationTriangleIcon className="h-8 w-8" />{" "}
-              {statusMessages[status]}
-            </div>
-          ) : null}
-        </div>
-        <div
-          className={`relative h-5 w-full rounded-full ${
-            status === "critical" ? "bg-critical" : "bg-gray-200"
-          }        ${status !== "good" ? "animate-pulse" : ""}`}
-        >
-          <div
-            className="h-5 rounded-full bg-green-600"
-            style={{
-              width: `${oxygenPercent}%`,
-            }}
-          ></div>
-          <div
-            className="absolute top-0 h-5 rounded-full bg-warning"
-            style={{
-              width: `${warningPercent}%`,
-            }}
-          ></div>
-        </div>
-      </div>
+      <OxygenLevel
+        oxygenLevel={oxygenLevel}
+        elapsedDuration={elapsedDuration}
+      />
       <Button type="submit" name="passId" value={openPassId} size="big">
         <div>Return Cadet</div>
       </Button>
