f49298695e47363827a984f36d77939df0f84901
The Batman <hectorscout@hotmail.com>
2022-10-14 09:56:23 -0600

    RocketIcon component

--- app/components/openPassDashboard.tsx
+++ app/components/openPassDashboard.tsx
@@ -1,5 +1,6 @@
 import { formatDurationDigital } from "~/utils/utils";
 import { Button } from "~/components/common/button";
+import { RocketIcon } from "~/components/common/rocketIcon";
 import { CheckBadgeIcon } from "@heroicons/react/24/outline";
 import { OxygenLevel } from "~/components/oxygenLevel";
 
@@ -9,24 +10,6 @@ interface OpenPassDashboardProps {
   oxygenLevel: number;
   isPersonal: boolean;
 }
-
-// TODO: move to a component (or fix), share with PassButton
-const rocketIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-    className="h-12 w-12"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"
-    />
-  </svg>
-);
 
 export const OpenPassDashboard = ({
   openPassId,
@@ -40,7 +23,7 @@ export const OpenPassDashboard = ({
     >
       <div className="flex justify-between">
         <h1 className="text-5xl">Current Space Walk</h1>
-        {isPersonal ? rocketIcon : <CheckBadgeIcon className="h-12 w-12" />}
+        {isPersonal ? <RocketIcon /> : <CheckBadgeIcon className="h-12 w-12" />}
       </div>
       <div className="flex items-center justify-center font-mono text-9xl">
         {formatDurationDigital(elapsedDuration)}
